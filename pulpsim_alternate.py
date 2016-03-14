172edc00f72517bceea86aa6f47b25c87a65b62c
MrMikeWolf <u12107949@tuks.co.za>
2016-03-14 16:08:32 +0200

    Added Diffusion with temperature dependence

--- pulpsim_alternate.py
+++ pulpsim_alternate.py
@@ -28,15 +28,17 @@ N = 1000
 dt = float(T) / float(N - 1)
 t_grid = numpy.array([n * dt for n in range(N)])
 
+
 # ### Specify System Parameters and the Reaction Term
 
 # State initial starting conditions
 
 # In[3]:
 
-D = 1.1952024623899164e-05 * 10  # diffusion rate (only hydroxide)
+def sigma_D(T):
+    D = 1.5e-2 * (T ** 0.5) * exp(-4870 / 1.9872 / T)
+    return D * dt / (2 * dx * dx)
 
-sigma = float(D * dt) / float((2. * dx * dx))
 
 # Specify the Initial Concentrations
 
@@ -45,7 +47,7 @@ sigma = float(D * dt) / float((2. * dx * dx))
 
 L = numpy.array([0.27 for i in range(0, J)])
 CC = numpy.array([0.776 for i in range(0, J)])
-CA = numpy.array([1.5625] + [0 for i in range(1, J)])
+CA = numpy.array([0.5] + [0 for i in range(1, J)])
 C = numpy.array([L, CC, CA])
 # Let us plot our initial condition for confirmation:
 
@@ -68,28 +70,29 @@ B_L = A_L
 A_C = numpy.identity(J)
 B_C = A_C
 
-A_CA = numpy.diagflat([-sigma for i in range(J - 1)], -1) + numpy.diagflat(
-    [1. + sigma] + [1. + 2. * sigma for i in range(J - 2)] + [1. + sigma]) + numpy.diagflat(
-    [-sigma for i in range(J - 1)], 1)
 
-B_CA = numpy.diagflat([sigma for i in range(J - 1)], -1) + numpy.diagflat(
-    [1. - sigma] + [1. - 2. * sigma for i in range(J - 2)] + [1. - sigma]) + numpy.diagflat(
-    [sigma for i in range(J - 1)], 1)
+def ACA(sigma):
+    return numpy.diagflat([-sigma for i in range(J - 1)], -1) + numpy.diagflat(
+        [1. + sigma] + [1. + 2. * sigma for i in range(J - 2)] + [1. + sigma]) + numpy.diagflat(
+        [-sigma for i in range(J - 1)], 1)
+
+
+def BCA(sigma):
+    return numpy.diagflat([sigma for i in range(J - 1)], -1) + numpy.diagflat(
+        [1. - sigma] + [1. - 2. * sigma for i in range(J - 2)] + [1. - sigma]) + numpy.diagflat(
+        [sigma for i in range(J - 1)], 1)
+
 
 # Solve the System Iteratively
 
 # In[21]:
 
-CS = 1.5625
+CS = 0.5
 SF = 1
 
 
-def f_vec(C, TC):
-    L, CC, CA = C
-
-    C[C < 0] = 0
-
-    if L[0] >= .25:
+def f_vec(L, CC, CA, TC):
+    if L[0] >= .22:
 
         dLdt = lambda L, CC, CA, TC: dt * (36.2 * TC ** 0.5 * exp(-4807.69 / TC)) * L
         dCCdt = lambda L, CC, CA, TC: dt * 2.53 * 36.2 * T ** 0.5 * exp(-4807.69 / TC) * L * (CA ** 0.11)
@@ -102,11 +105,11 @@ def f_vec(C, TC):
         dLdt = lambda L, CC, CA, TC: dt * (
             exp(35.19 - 17200 / TC) * CA + (exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4))) * L
         dCCdt = lambda L, CC, CA, TC: dt * (
-            0.47(exp(35.19 - 17200 / TC) * CA + (exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4)))) * L
+            0.47*(exp(35.19 - 17200 / TC) * CA + (exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4)))) * L
         dCAdt = lambda L, CC, CA, TC: dt * SF * (-4.78e-3 * (exp(35.19 - 17200 / TC) * CA * L +
-                                                             exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4)*L)
-                                                 + 1.81e-2 * 0.47 * (exp(35.19 - 17200 / TC) * CA* L + (
-            exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4)*L)))
+                                                             exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4) * L)
+                                                 + 1.81e-2 * 0.47 * (exp(35.19 - 17200 / TC) * CA * L + (
+            exp(29.23 - 14400 / TC) * (CA ** 0.5) * (CS ** 0.4) * L)))
 
     else:
 
@@ -156,7 +159,11 @@ CA_record.append(CA)
 for ti in range(1, N):
     TC = temp(ti)
 
-    vec_L, vec_CC, vec_CA = f_vec(C, TC)
+    vec_L, vec_CC, vec_CA = f_vec(L,CC,CA, TC)
+
+    sigma = sigma_D(TC)
+    A_CA = ACA(sigma)
+    B_CA = BCA(sigma)
 
     L_new = numpy.linalg.solve(A_L, B_L.dot(L) - vec_L(L, CC, CA, TC))
     CC_new = numpy.linalg.solve(A_C, B_C.dot(CC) - vec_CC(L, CC, CA, TC))
